e041a7b99ee1de6106bc6c2b37cf4a2410f45a41
Alex Zhukov <zhukov.alex@gmail.com>
2015-06-14 16:58:43 +0300

    support resampling when encoding low bitrates

--- src/js/Lame.js
+++ src/js/Lame.js
@@ -1682,6 +1682,164 @@ function Lame() {
         this.num_used = 0;
     }
 
+    /**
+     * Greatest common divisor.
+     * <p>
+     * Joint work of Euclid and M. Hendry
+     */
+    function gcd(i, j) {
+        return j != 0 ? gcd(j, i % j) : i;
+    }
+
+    /**
+     * Resampling via FIR filter, blackman window.
+     */
+    function blackman(x, fcn, l) {
+        /*
+         * This algorithm from: SIGNAL PROCESSING ALGORITHMS IN FORTRAN AND C
+         * S.D. Stearns and R.A. David, Prentice-Hall, 1992
+         */
+        var wcn = (Math.PI * fcn);
+
+        x /= l;
+        if (x < 0)
+            x = 0;
+        if (x > 1)
+            x = 1;
+        var x2 = x - .5;
+
+        var bkwn = 0.42 - 0.5 * Math.cos(2 * x * Math.PI) + 0.08 * Math.cos(4 * x * Math.PI);
+        if (Math.abs(x2) < 1e-9)
+            return (wcn / Math.PI);
+        else
+            return (bkwn * Math.sin(l * wcn * x2) / (Math.PI * l * x2));
+    }
+
+    function fill_buffer_resample(gfp, outbuf, outbufPos, desired_len, inbuf, in_bufferPos, len, num_used, ch) {
+        var gfc = gfp.internal_flags;
+        var i, j = 0, k;
+        /* number of convolution functions to pre-compute */
+        var bpc = gfp.out_samplerate
+            / gcd(gfp.out_samplerate, gfp.in_samplerate);
+        if (bpc > LameInternalFlags.BPC)
+            bpc = LameInternalFlags.BPC;
+
+        var intratio = (Math.abs(gfc.resample_ratio
+            - Math.floor(.5 + gfc.resample_ratio)) < .0001) ? 1 : 0;
+        var fcn = 1.00 / gfc.resample_ratio;
+        if (fcn > 1.00)
+            fcn = 1.00;
+        var filter_l = 31;
+        if (0 == filter_l % 2)
+            --filter_l;
+        /* must be odd */
+        filter_l += intratio;
+        /* unless resample_ratio=int, it must be even */
+
+        var BLACKSIZE = filter_l + 1;
+        /* size of data needed for FIR */
+
+        if (gfc.fill_buffer_resample_init == 0) {
+            gfc.inbuf_old[0] = new_float(BLACKSIZE);
+            gfc.inbuf_old[1] = new_float(BLACKSIZE);
+            for (i = 0; i <= 2 * bpc; ++i)
+                gfc.blackfilt[i] = new_float(BLACKSIZE);
+
+            gfc.itime[0] = 0;
+            gfc.itime[1] = 0;
+
+            /* precompute blackman filter coefficients */
+            for (j = 0; j <= 2 * bpc; j++) {
+                var sum = 0.;
+                var offset = (j - bpc) / (2. * bpc);
+                for (i = 0; i <= filter_l; i++)
+                    sum += gfc.blackfilt[j][i] = blackman(i - offset, fcn,
+                        filter_l);
+                for (i = 0; i <= filter_l; i++)
+                    gfc.blackfilt[j][i] /= sum;
+            }
+            gfc.fill_buffer_resample_init = 1;
+        }
+
+        var inbuf_old = gfc.inbuf_old[ch];
+
+        /* time of j'th element in inbuf = itime + j/ifreq; */
+        /* time of k'th element in outbuf = j/ofreq */
+        for (k = 0; k < desired_len; k++) {
+            var time0;
+            var joff;
+
+            time0 = k * gfc.resample_ratio;
+            /* time of k'th output sample */
+            j = 0 | Math.floor(time0 - gfc.itime[ch]);
+
+            /* check if we need more input data */
+            if ((filter_l + j - filter_l / 2) >= len)
+                break;
+
+            /* blackman filter. by default, window centered at j+.5(filter_l%2) */
+            /* but we want a window centered at time0. */
+            var offset = (time0 - gfc.itime[ch] - (j + .5 * (filter_l % 2)));
+            assert(Math.abs(offset) <= .501);
+
+            /* find the closest precomputed window for this offset: */
+            joff = 0 | Math.floor((offset * 2 * bpc) + bpc + .5);
+            var xvalue = 0.;
+            for (i = 0; i <= filter_l; ++i) {
+                var j2 = i + j - filter_l / 2;
+                var y;
+                assert(j2 < len);
+                assert(j2 + BLACKSIZE >= 0);
+                y = (j2 < 0) ? inbuf_old[BLACKSIZE + j2] : inbuf[in_bufferPos
+                + j2];
+                xvalue += y * gfc.blackfilt[joff][i];
+            }
+            outbuf[outbufPos + k] = xvalue;
+        }
+
+        /* k = number of samples added to outbuf */
+        /* last k sample used data from [j-filter_l/2,j+filter_l-filter_l/2] */
+
+        /* how many samples of input data were used: */
+        num_used.num_used = Math.min(len, filter_l + j - filter_l / 2);
+
+        /*
+         * adjust our input time counter. Incriment by the number of samples
+         * used, then normalize so that next output sample is at time 0, next
+         * input buffer is at time itime[ch]
+         */
+        gfc.itime[ch] += num_used.num_used - k * gfc.resample_ratio;
+
+        /* save the last BLACKSIZE samples into the inbuf_old buffer */
+        if (num_used.num_used >= BLACKSIZE) {
+            for (i = 0; i < BLACKSIZE; i++)
+                inbuf_old[i] = inbuf[in_bufferPos + num_used.num_used + i
+                - BLACKSIZE];
+        } else {
+            /* shift in num_used.num_used samples into inbuf_old */
+            var n_shift = BLACKSIZE - num_used.num_used;
+            /*
+             * number of samples to
+             * shift
+             */
+
+            /*
+             * shift n_shift samples by num_used.num_used, to make room for the
+             * num_used new samples
+             */
+            for (i = 0; i < n_shift; ++i)
+                inbuf_old[i] = inbuf_old[i + num_used.num_used];
+
+            /* shift in the num_used.num_used samples */
+            for (j = 0; i < BLACKSIZE; ++i, ++j)
+                inbuf_old[i] = inbuf[in_bufferPos + j];
+
+            assert(j == num_used.num_used);
+        }
+        return k;
+        /* return the number samples created at the new samplerate */
+    }
+
     function fill_buffer(gfp, mfbuf, in_buffer, in_bufferPos, nsamples, io) {
         var gfc = gfp.internal_flags;
 
